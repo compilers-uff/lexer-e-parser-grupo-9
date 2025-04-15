9507c25ad9697b01179b160fc2f29b69e39d22a9
LucasLMPinto <lucaslmp@id.uff.br>
2025-04-15 15:08:13 -0300

    Update bad.py

--- src/test/data/pa1/student_contributed/bad.py
+++ src/test/data/pa1/student_contributed/bad.py
@@ -1,21 +1,32 @@
-x int = 5      # erro: falta ":"
-y: [int] = [1, 2, 3
+# erro 1: Falta ":"
+# em atribuiçoes
+x int = 5
 
+# em declarações de função
 def soma(a int b int) -> int
     resultado: int = a + b
     return resultado
 
+# e em classe
 class Animal(object)
-    nome: str = "Bicho"
+    nome: str = "foo"
 
-    def falar(self: "Animal") -> str:
-        return "???"
+    def falar(self: "fii") -> str:
+        return "fuuu"
 
+#Erro 2: Declaração de funçao sem fim/retorno
 def semFim(x: int) -> int:   
-                               
-# erro: uso incorreto de indentação
+
+
+# erro 3: Conchetes não fechado
+y: [int] = [1, 2, 3                               
+            
+# erro 4: Uso incorreto de indentação
 if x > 5:
 print("Erro de indentação")
 
-# erro de operador
+# erro 5:  De operador
 z: int = 5 + * 3
+
+# erro 6: Aspas não fechada
+teste: str = "Faaaa
